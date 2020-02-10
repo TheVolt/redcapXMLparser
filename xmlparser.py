064bd10c6db45ee966fff6c37dcec25fc5b0c083
cabxr8 <40616838+cabxr8@users.noreply.github.com>
2020-02-10 18:22:21 -0500

    Add files via upload

---
+++ xmlparser.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 21 08:37:54 2019
+
+to parse xml redcap files into data dictionaries....
+
+@author: cabxr
+"""
+
+# import libraries
+import os
+import csv
+import xml.etree.ElementTree as ET
+
+# set working directory
+os.chdir('C:\\Users\\cabxr\\Desktop\\Redcap Surveys-20190820T145856Z-001\\Redcap Surveys')
+
+# check working diretory
+print(os.getcwd())
+
+# parse xml files
+docTree = ET.parse("6mi.xml")
+
+# get root items from xml
+root = docTree.getroot()
+
+# check if xml read
+if (docTree):
+    print("file loaded")
+else:
+    print("file not loaded")
+
+# open a csv file for writing
+xml_data = open('6mi.csv', 'w', encoding='utf-8')
+
+# create csv writer object
+csvwriter = csv.writer(xml_data, lineterminator='\n')
+
+# create csv row headings
+xml_head = []
+
+xml_head.append("Variable / Field Name")
+xml_head.append("Form Name")
+#xml_head.append("Section Header")
+xml_head.append("Field Type")
+xml_head.append("Field Label")
+xml_head.append("Choices, Calculations, OR Slider Labels")
+#xml_head.append("Field Note")
+#xml_head.append("Text Validation Type OR Show Slider")
+#xml_head.append("Number")
+#xml_head.append("Text Validation Min")
+#xml_head.append("Text Validation Max")
+#xml_head.append("Identifier?")
+#xml_head.append("Branching Logic (Show field only if...")
+#xml_head.append("Required Field?")
+#xml_head.append("Custom Alignment")
+#xml_head.append("Question Number (surveys only")
+#xml_head.append("Matrix Group Name")
+csvwriter.writerow(xml_head)
+
+
+# iterate through roots
+for member in root.findall('.//{http://www.cdisc.org/ns/odm/v1.3}ItemDef'):
+    # create an empty list for variables information
+    xml_variables = []
+    
+    var=member.attrib['Name']
+    xml_variables.append(var)
+    
+    formName='6MonthImmediateSurvey'
+    xml_variables.append(formName)
+    
+    fieldType=member.attrib['{https://projectredcap.org}FieldType']
+    if fieldType == 'select':
+        xml_variables.append('dropdown')
+    else:
+        xml_variables.append(fieldType)
+    
+    fieldLabel=member.find('.//{http://www.cdisc.org/ns/odm/v1.3}TranslatedText').text
+    xml_variables.append(fieldLabel)
+    
+    codelist=member.find('.//{http://www.cdisc.org/ns/odm/v1.3}CodeListRef')
+    if codelist == None:
+        choices = ''
+    else:
+        choicesCode = codelist.attrib['CodeListOID']
+        choicesStr = './/*[@OID=' + "'" + choicesCode + "'" + ']'
+        #print(choicesStr)
+        
+        choices = []
+        
+        for options in root.findall(choicesStr):
+            for items in options.findall('.//{http://www.cdisc.org/ns/odm/v1.3}CodeListItem'):
+                codeVal = items.attrib['CodedValue']
+                codeText = items.find('.//{http://www.cdisc.org/ns/odm/v1.3}TranslatedText').text
+                itemText = codeVal + ', ' + codeText
+                
+                choices.append(itemText)
+                
+    choiceVals = ' | '.join(choices)
+    xml_variables.append(choiceVals)
+    
+    csvwriter.writerow(xml_variables)
+                
+#for i in root:
+ #   for j in i:
+  #      for k in j:
+   #         print(k.attrib)
+            #for l in k:
+    #            for m in l:
+     #               print(m)
+                    #print(m.text)
+
+xml_data.close()
